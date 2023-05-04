4fe3ec9178daf19be1f96888e51114da0f8cfb8a
sumit6675 <sumitchimkar0@gmail.com>
2023-05-04 16:07:20 +0530

    Create a Basic Angular Component

--- Day4/my-first-project/src/app/app.module.ts
+++ Day4/my-first-project/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { coursesComponents } from './course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    coursesComponents
   ],
   imports: [
     BrowserModule,
---
+++ Day4/my-first-project/src/app/course.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+@Component({
+    selector:'courses',
+    template:'<h2>Angular Practice 1</h2>'
+})
+export class coursesComponents{
+}
