c418f69948725c2f50a94f65cadb8b9dea8e3a04
sumit6675 <sumitchimkar0@gmail.com>
2023-05-04 15:53:50 +0530

    typescript practice

---
+++ Typescript/main.js
@@ -0,0 +1,12 @@
+// function log(msg){
+//     console.log('msg', msg)
+// }
+// var msg="Hello Sumit"
+// log(msg)
+var drawPoints = function (points) {
+    console.log(points);
+};
+drawPoints({
+    x: 1,
+    y: 2
+});
---
+++ Typescript/main.ts
@@ -0,0 +1,29 @@
+// function log(msg){
+//     console.log('msg', msg)
+// }
+// var msg="Hello Sumit"
+// log(msg)
+
+//Type assertions
+
+// let msg;
+// msg = "Hello Sumit";
+// let endsWithT = (<string>msg).endsWith("t"); //one way
+// let alternateWay = (msg as string).endsWith("t"); //one way
+// console.log(endsWithT);
+// console.log(alternateWay);
+
+// interface Point {
+//   x: number;
+//   y: number;
+// }
+
+// let drawPoints = (points:Point) => {
+//     console.log(points);
+// };
+// drawPoints({
+//   x: 1,
+//   y: 2,
+// });
+
+
